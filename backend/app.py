4e7acc843dbc7ac7c90efca12751c1a4c28668df
Ayesha Maqsood <ayeshamaqsood1310@gmail.com>
2026-04-30 21:25:24 +0500

    simplify app.py

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from pdf_generators.sacs import generate_sacs_pdf
 from pdf_generators.tcc import generate_tcc_pdf
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,41 +11,24 @@ CORS(app)
 def health():
     return jsonify({'status': 'ok'})
 
-@app.route('/generate/sacs', methods=['POST', 'OPTIONS'])
+@app.route('/generate/sacs', methods=['POST'])
 def generate_sacs():
-    if request.method == 'OPTIONS':
-        response = make_response()
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        return response, 200
     data = request.get_json()
-    pdf_buffer = generate_sacs_pdf(data)
-    pdf_bytes = pdf_buffer.read()
+    pdf_bytes = generate_sacs_pdf(data).read()
     response = make_response(pdf_bytes)
-    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'inline; filename=SACS.pdf'
-    response.headers['Content-Length'] = len(pdf_bytes)
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
-@app.route('/generate/tcc', methods=['POST', 'OPTIONS'])
+@app.route('/generate/tcc', methods=['POST'])
 def generate_tcc():
-    if request.method == 'OPTIONS':
-        response = make_response()
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-        response.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
-        return response, 200
     data = request.get_json()
-    pdf_buffer = generate_tcc_pdf(data)
-    pdf_bytes = pdf_buffer.read()
+    pdf_bytes = generate_tcc_pdf(data).read()
     response = make_response(pdf_bytes)
-    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'inline; filename=TCC.pdf'
-    response.headers['Content-Length'] = len(pdf_bytes)
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
