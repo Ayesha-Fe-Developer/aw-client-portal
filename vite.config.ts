e9eff59f2b23f36e61a21714213f3ed117c594d0
Ayesha Maqsood <ayeshamaqsood1310@gmail.com>
2026-04-30 21:07:55 +0500

    fix vite build config for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        configure: (proxy) => {
-          proxy.on("error", (err) => {
-            console.log("proxy error", err);
-          });
-          proxy.on("proxyReq", (_proxyReq, req) => {
-            console.log("Sending Request:", req.method, req.url);
-          });
-          proxy.on("proxyRes", (proxyRes, req) => {
-            console.log("Received Response:", proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
   },
 });
