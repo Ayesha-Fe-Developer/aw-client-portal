487b5544bf03b5717f1fb6a32bd624a7d82542a4
Ayesha Maqsood <ayeshamaqsood1310@gmail.com>
2026-04-30 21:38:06 +0500

    fix CORS manually

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,28 @@
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
 from pdf_generators.sacs import generate_sacs_pdf
 from pdf_generators.tcc import generate_tcc_pdf
 import os
 
 app = Flask(__name__)
-CORS(app)
+
+def corsify(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
+
+@app.before_request
+def handle_preflight():
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        return response
 
 @app.route('/health', methods=['GET'])
 def health():
-    return jsonify({'status': 'ok'})
+    return corsify(make_response(jsonify({'status': 'ok'})))
 
 @app.route('/generate/sacs', methods=['POST'])
 def generate_sacs():
@@ -17,8 +30,7 @@ def generate_sacs():
     pdf_bytes = generate_sacs_pdf(data).read()
     response = make_response(pdf_bytes)
     response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return corsify(response)
 
 @app.route('/generate/tcc', methods=['POST'])
 def generate_tcc():
@@ -26,8 +38,7 @@ def generate_tcc():
     pdf_bytes = generate_tcc_pdf(data).read()
     response = make_response(pdf_bytes)
     response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
+    return corsify(response)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
